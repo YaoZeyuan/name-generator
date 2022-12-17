b3f2351950929f6ae97e521517effd0351ba0eff
yaozeyuan001 <yaozeyuan001@ke.com>
2022-12-17 12:56:17 +0800

    feat: 更新构建脚本

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     reactRefresh()
   ],
