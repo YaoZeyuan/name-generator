dd3b508712034e306c0726aff3017d4fba120006
yaozeyuan001 <yaozeyuan001@ke.com>
2022-12-19 00:05:09 +0800

    feat: 支持排除指定同音字

--- src/index.tsx
+++ src/index.tsx
@@ -8,22 +8,15 @@ import {
   Const_Level_2_Pinyin,
 } from "./const/pinyin_移除不好的字";
 
-import { Button } from "antd";
+import { Button, Input } from "antd";
 import * as utils from "@/utils";
 import * as Types from "@/types/index";
 import NameList from "@/component/name_list";
 
-// 引入样式
-
 const Const_Storage_Key = "name_storage";
 const Const_Storage_姓氏_Key = `${Const_Storage_Key}_family_name`;
-const Const_Storage_人名_第一个字_Key = `${Const_Storage_Key}_Name_Char_1`;
-const Const_Storage_人名_第二个字_Key = `${Const_Storage_Key}_Name_Char_2`;
+const Const_Storage_需过滤字列表_Key = `${Const_Storage_Key}_Need_Fileter_Char`;
 const Const_Storage_Char_Leve_Key = `${Const_Storage_Key}_Char_Level`;
-
-const Const_Pinyin_Menu_Option_Level_无音调拼音 = "pinyin" as const;
-const Const_Pinyin_Menu_Option_Level_带音调拼音 = "带音调拼音" as const;
-const Const_Pinyin_Menu_Option_Level_可选字 = "可选字" as const;
 
 // 所有可选字列表
 let TotalCharOptionList: Types.Type_Char_Option[] = [];
@@ -62,28 +55,20 @@ for (let pinyin of Object.keys(Pinyin_Database_Map)) {
 }
 
 let default_input_姓氏_str = localStorage.getItem(Const_Storage_姓氏_Key);
-let default_select_人名_第一个字_str = localStorage.getItem(
-  Const_Storage_人名_第一个字_Key
+let default_input_需过滤字列表_str = localStorage.getItem(
+  Const_Storage_需过滤字列表_Key
 );
-let default_select_人名_第二个字_str =
-  localStorage.getItem(Const_Storage_人名_第二个字_Key) || "";
 
 let default_input_姓氏 = "";
-let default_select_人名_第一个字 = TotalCharOptionList[0];
-let default_select_人名_第二个字 = TotalCharOptionList[0];
 try {
   if (default_input_姓氏_str !== null) {
     default_input_姓氏 = JSON.parse(default_input_姓氏_str);
   }
 } catch (e) {}
+let default_input_需过滤字列表 = "";
 try {
-  if (default_select_人名_第一个字_str !== null) {
-    default_select_人名_第一个字 = JSON.parse(default_select_人名_第一个字_str);
-  }
-} catch (e) {}
-try {
-  if (default_select_人名_第一个字_str !== null) {
-    default_select_人名_第二个字 = JSON.parse(default_select_人名_第二个字_str);
+  if (default_input_需过滤字列表_str !== null) {
+    default_input_需过滤字列表 = JSON.parse(default_input_需过滤字列表_str);
   }
 } catch (e) {}
 
@@ -113,6 +98,8 @@ const store = proxy<{
 
 export default () => {
   let [input_姓氏, set_input_姓氏] = useState(default_input_姓氏);
+  let [input_需过滤字列表, set_input_需过滤字列表] =
+    useState(default_input_需过滤字列表);
 
   let storeSnapshot = useSnapshot(store);
 
@@ -120,13 +107,27 @@ export default () => {
     let nameList: Types.Type_Name[] = [];
     let char_姓氏_最后一个字 = 总字库[char_姓氏?.[char_姓氏.length - 1] || ""];
 
-    let new_char_1_optionList = TotalCharOptionList.filter((item) => {
+    let needFileterCharList = input_需过滤字列表.split("");
+    let needFileterPinyinSet = new Set();
+    for (let needFileterChar of needFileterCharList) {
+      let pinyin = 总字库[needFileterChar]?.pinyin;
+      if (pinyin) {
+        needFileterPinyinSet.add(pinyin);
+      }
+    }
+
+    // 过滤掉所有同音字
+    let newTotalCharOptionList = TotalCharOptionList.filter((item) => {
+      return needFileterPinyinSet.has(item.char_item.pinyin) === false;
+    });
+
+    let new_char_1_optionList = newTotalCharOptionList.filter((item) => {
       let isLegal = utils.isCharLegal(char_姓氏_最后一个字, item.char_item);
       return isLegal;
     });
 
     for (let char1 of new_char_1_optionList) {
-      let new_char_2_optionList = TotalCharOptionList.filter((item) => {
+      let new_char_2_optionList = newTotalCharOptionList.filter((item) => {
         let isLegal = utils.isCharLegal(char1.char_item, item.char_item);
         return isLegal;
       });
@@ -139,7 +140,6 @@ export default () => {
         nameList.push(name);
       }
     }
-    console.log("nameList.length => ", nameList.length);
     return nameList;
   };
 
@@ -159,6 +159,25 @@ export default () => {
             set_input_姓氏(inputValue);
           }}
         ></input>
+      </div>
+      <div>
+        需要避开的同音字(例如父母姓名/亲属姓名)
+        <Input.TextArea
+          value={input_需过滤字列表}
+          onChange={(e) => {
+            let inputValue = e.target.value;
+            inputValue = inputValue.trim();
+            // 去重
+            inputValue = [...new Set(inputValue.split("")).values()].join("");
+            localStorage.setItem(
+              Const_Storage_需过滤字列表_Key,
+              JSON.stringify(inputValue)
+            );
+            set_input_需过滤字列表(inputValue);
+          }}
+        ></Input.TextArea>
+      </div>
+      <div>
         <Button
           onClick={function () {
             let nameList = generateAllNameList(input_姓氏);
@@ -168,7 +187,7 @@ export default () => {
             store.nameList = nameList.slice(0, storeSnapshot.maxDisplayItem);
           }}
         >
-          生成所有可能的名字
+          点击生成所有可能的名字
         </Button>
       </div>
 
