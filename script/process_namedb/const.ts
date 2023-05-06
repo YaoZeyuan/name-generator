2def4f6285cda0a15cdc01de3c8ba42adcd14545
yaozeyuan001 <yaozeyuan001@ke.com>
2023-05-06 21:36:31 +0800

    feat: 对数据库进行分类

--- script/process_namedb/const.ts
+++ script/process_namedb/const.ts
@@ -3,4 +3,7 @@ import * as path from "path";
 const basePath = path.resolve(__dirname, "..", "..");
 
 export const Raw_已知人名_Path = path.resolve(basePath, "database/raw");
-export const CharDb_Uri = path.resolve(basePath, "database/json/char_db.json");
+export const CharDb_raw_人名_Uri = path.resolve(
+  basePath,
+  "database/json/raw_person_name_char_db.json.json"
+);
