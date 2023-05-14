8b859584fec37b6dbe9ef30ec76ef56ed44ae784
yaozeyuan001 <yaozeyuan001@ke.com>
2023-05-15 00:54:36 +0800

    feat:  备份代码

--- script/common/db.ts
+++ script/common/db.ts
@@ -1,9 +1,23 @@
 import * as path from "path";
 import * as Type from "./type";
 
+import Only_Char_Min_1 from "@/database/char_db/min_1_olny_char.json";
+import Only_Char_Min_5 from "@/database/char_db/min_5_olny_char.json";
+import Only_Char_Min_10 from "@/database/char_db/min_10_olny_char.json";
+import Only_Char_Min_50 from "@/database/char_db/min_50_olny_char.json";
+import Only_Char_Min_100 from "@/database/char_db/min_100_olny_char.json";
+
+// import PinyinDb_Min_1 from "@/database/pinyin_db/zd_name_pinyin_db_min_1.json";
+// import PinyinDb_Min_5 from "@/database/pinyin_db/zd_name_pinyin_db_min_5.json";
+// import PinyinDb_Min_10 from "@/database/pinyin_db/zd_name_pinyin_db_min_10.json";
+// import PinyinDb_Min_50 from "@/database/pinyin_db/zd_name_pinyin_db_min_50.json";
+// import PinyinDb_Min_100 from "@/database/pinyin_db/zd_name_pinyin_db_min_100.json";
+
 import CharDb_主动规定发音的多音字列表 from "@/database/char_db/主动规定发音的多音字列表.json";
 import Char_List_不含多音字 from "@/database/char_db/zd_without_muilt_tone_only_char.json";
 import RawPinyinList from "@/database/char_db/raw_pinyin_list.json";
+
+// 基于单音字列表, 构建单音字-拼音数据库
 
 let charDb_不含多音字: Type.DB_Char_With_Pinyin = {};
 let set不含多音字 = new Set(Char_List_不含多音字);
@@ -20,3 +34,5 @@ for (let pinyinConfig of CharDb_主动规定发音的多音字列表) {
 }
 
 export const CharDb_不含多音字 = charDb_不含多音字;
+
+// 基于姓名出现频率列表, 构建姓名频率-拼音数据库
