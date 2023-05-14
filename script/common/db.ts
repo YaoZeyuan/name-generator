d64eaacd7d12bbc4512f6070a9191bd8e06797a9
yaozeyuan001 <yaozeyuan001@ke.com>
2023-05-15 00:48:59 +0800

    feat: 将字符数据库改为通过运算产生, 以降低打包体积

--- script/common/db.ts
+++ script/common/db.ts
@@ -1,6 +1,22 @@
 import * as path from "path";
 import * as Type from "./type";
 
-import CharDb_不含多音字 from "@/database/char_db/zd_without_muilt_tone_char_db.json";
 import CharDb_主动规定发音的多音字列表 from "@/database/char_db/主动规定发音的多音字列表.json";
-import CharDb_所有支持汉字 from "@/database/char_db/db_all_char_map.json";
+import Char_List_不含多音字 from "@/database/char_db/zd_without_muilt_tone_only_char.json";
+import RawPinyinList from "@/database/char_db/raw_pinyin_list.json";
+
+let charDb_不含多音字: Type.DB_Char_With_Pinyin = {};
+let set不含多音字 = new Set(Char_List_不含多音字);
+for (let pinyin of RawPinyinList) {
+  if (set不含多音字.has(pinyin.char)) {
+    // @ts-ignore
+    charDb_不含多音字[pinyin.char] = pinyin;
+  }
+}
+// 将特定多音字按照起名中实际使用的发音视为单音字, 补充到CharDB字典中
+for (let pinyinConfig of CharDb_主动规定发音的多音字列表) {
+  // @ts-ignore
+  charDb_不含多音字[pinyinConfig.char] = pinyinConfig;
+}
+
+export const CharDb_不含多音字 = charDb_不含多音字;
